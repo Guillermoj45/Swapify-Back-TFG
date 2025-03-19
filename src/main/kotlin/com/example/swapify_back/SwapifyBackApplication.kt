257541ecd8c1798b75050f0bef484114719a6d39
JaviMontes <fmontesestrabon@safareyes.es>
2025-03-19 18:24:45 +0100

    primer commit

---
+++ src/main/kotlin/com/example/swapify_back/SwapifyBackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.swapify_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwapifyBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwapifyBackApplication>(*args)
+}
