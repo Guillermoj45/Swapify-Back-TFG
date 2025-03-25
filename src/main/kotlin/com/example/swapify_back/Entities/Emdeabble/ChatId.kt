6971a184ed8eb80e3367933bcb085778f909e597
JaviMontes <fmontesestrabon@safareyes.es>
2025-03-25 18:05:29 +0100

    Pequeño cambio

--- src/main/kotlin/com/example/swapify_back/Entities/Emdeabble/ChatId.kt
+++ src/main/kotlin/com/example/swapify_back/Entities/Emdeabble/ChatId.kt
@@ -5,14 +5,8 @@ import java.io.Serializable
 import java.util.*
 
 
-class ChatId(): Serializable {
-    var user1: UUID? = null
-    var user2: UUID? = null
-    var product: UUID? = null
-
-    constructor(user1: UUID, user2: UUID, product: UUID): this() {
-        this.user1 = user1
-        this.user2 = user2
-        this.product = product
-    }
-}
+class ChatId(
+    var user1: UUID,
+    var user2: UUID,
+    var product: UUID
+): Serializable
